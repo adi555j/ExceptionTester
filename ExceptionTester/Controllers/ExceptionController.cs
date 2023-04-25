db4f26ad6f894a2ac12d248ab24003fe3472ac4c
adi555j <adi555j@gmail.com>
2023-04-25 12:08:51 +0530

    Add project files.

---
+++ ExceptionTester/Controllers/ExceptionController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System.ComponentModel;
+
+namespace ExceptionTester.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExceptionController : Controller
+    {
+        [HttpGet]
+        [Route("NotImplementedException")]
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+            return View();
+        }
+        [HttpGet]
+        [Route("InvalidCastException")]
+        public IActionResult InvalidCastException()
+        {
+            throw new InvalidCastException();
+            return View();
+        }
+        [HttpGet]
+        [Route("InvalidOperationException")]
+        public IActionResult InvalidOperationException()
+        {
+            throw new InvalidOperationException();
+            return View();
+        }
+
+        [HttpGet]
+        [Route("InvalidDataException")]
+        public IActionResult InvalidDataException()
+        {
+            throw new InvalidDataException();
+            return View();
+        }
+
+        [HttpGet]
+        [Route("InvalidAsynchronousStateException")]
+        public IActionResult InvalidAsynchronousStateException()
+        {
+            throw new InvalidAsynchronousStateException();
+            return View();
+        }
+
+        [HttpGet]
+        [Route("DivideByZero")]
+        public IActionResult DivideByZero()
+        {
+            var y = 0;
+            var x = 5 / y;
+            return View();
+        }
+    }
+}
